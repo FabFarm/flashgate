78e3ac2fdf431fa472bf26e6a5c83ace6da49196
lavanya-m-k <lavanyamk97@gmail.com>
2024-04-23 10:42:35 +0100

    added short pulse logic

--- firmware/src/flashgate/main.cpp
+++ firmware/src/flashgate/main.cpp
@@ -2,16 +2,94 @@
 #include "WifiUtils.h"
 #include "HttpServer.h"
 #include "FileSystem.h"
+#include "movingAvg.h"
 
-void setup()
+
+#define LIGHT_SENSOR_PIN 34 // ESP32 pin GIOP36 (ADC0)
+#define GATE_RELAY_PIN 21 
+#define N 10
+#define SHORT_PULSE_WINDOW 4
+#define LONG_PULSE_WINDOW 6
+#define THRESHOLD 300.0
+
+uint8_t byte_index = 0;
+uint16_t history[N];
+
+void create_samples(uint16_t sample)
 {
-  Serial.begin(9600);
-  setupFileSystem();
-  setupWifi();
-  setupHttpServer();
+  uint16_t previous = history[byte_index] ;  // get oldest from buffer
+  history[byte_index] = sample ;  // insert newest
+  byte_index++ ;    // move pointer circularly
+  // use milis function instead of delay(way )
+    if (byte_index >= N){
+    byte_index = 0 ;
+  }
 }
 
-void loop()
-{
-  // Nothing here
+
+// Function to compare averages of two sets of N elements each
+int detectPulses(int size) {
+    // Calculate the average of the first N elements
+    double avg1 = 0;
+    for (int i = 0; i < N; i++) {
+        avg1 += history[i];
+    }
+    avg1 /= (double)N;
+
+    // Calculate the average of the next 10 elements
+    double avg2 = 0;
+    for (int i = N-size; i < N; i++) {
+        avg2 += history[i];
+    }
+    avg2 /= double(size);
+    Serial.printf("Avg1:%.3f\n", avg1);
+    Serial.printf("Avg2:%.3f\n", avg2);
+    
+    double diff = avg1 - avg2;
+    Serial.printf("Diff:%.3f\n", diff);
+
+    // Compare averages
+    if (diff >=THRESHOLD)
+        return 1;
+    else
+        return 0;
+}
+
+
+
+
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  pinMode(GATE_RELAY_PIN,OUTPUT);
+  pinMode(LIGHT_SENSOR_PIN,INPUT);
+  Serial.begin(115200);
+  digitalWrite(GATE_RELAY_PIN, LOW);
+}
+
+void loop() {
+  // reads the input on analog pin (value between 0 and 4095)
+  int sensorData = analogRead(LIGHT_SENSOR_PIN);
+  uint16_t sample = (uint16_t) sensorData;
+  unsigned long startTime = millis();
+
+  create_samples (sample) ;
+
+  Serial.printf("Raw:%d\n", sensorData);
+  
+  if (detectPulses(SHORT_PULSE_WINDOW)==1){
+    // Serial.println("HIGH");
+    Serial.printf("Gate Open");
+    digitalWrite(GATE_RELAY_PIN, HIGH); // turn on LED
+    while (millis() - startTime < 100) {
+    // Do nothing, just wait
+    }
+  }
+  else {
+    digitalWrite(GATE_RELAY_PIN, LOW);  // turn off LED
+  }
+  
+  
+  while (millis() - startTime < 50) {
+    // Do nothing, just wait
+  }
 }
